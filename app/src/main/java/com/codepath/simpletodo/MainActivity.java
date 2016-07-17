c98292a2037375948a133d609dbca425a76c957a
sodacarbon <sodacarbon@gmail.com>
2016-07-17 12:56:32 +0700

    2.1 correction: move variables outside function

--- app/src/main/java/com/codepath/simpletodo/MainActivity.java
+++ app/src/main/java/com/codepath/simpletodo/MainActivity.java
@@ -8,15 +8,18 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView lvItems;
+    ArrayList<String> items ;
+    ArrayAdapter<String> itemsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView lvItems = (ListView) findViewById(R.id.lvItems);
-        ArrayList items = new ArrayList<>();
-        ArrayAdapter itemsAdapter = new ArrayAdapter<>(this,
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
         items.add("First Item");
